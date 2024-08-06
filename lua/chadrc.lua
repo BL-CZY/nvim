2d600e9e37f9302703a0e218f9fcd3e3785d828a
BL-CZY <jiguangllsw@gmail.com>
2024-08-06 11:01:20 -0700

    configured terminal
    looks good

--- lua/chadrc.lua
+++ lua/chadrc.lua
@@ -6,11 +6,23 @@ local M = {}
 
 M.base46 = {
 	theme = "catppuccin",
-
 	-- hl_override = {
 	-- 	Comment = { italic = true },
 	-- 	["@comment"] = { italic = true },
 	-- },
+}
+
+M.term = {
+  winopts = { number = false, relativenumber = false },
+  sizes = { sp = 0.3, vsp = 0.2, ["bo sp"] = 0.3, ["bo vsp"] = 0.2 },
+  float = {
+    relative = "editor",
+    row = 0.2,
+    col = 0.25,
+    width = 0.5,
+    height = 0.5,
+    border = "single",
+  },
 }
 
 return M
--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -25,6 +25,38 @@ return {
     "mg979/vim-visual-multi",
     branch = "master",
     lazy = false,
+  },
+
+  {
+    "NvChad/nvterm",
+    config = function ()
+        require("nvterm").setup({
+          terminals = {
+            shell = vim.o.shell,
+            list = {},
+            type_opts = {
+              float = {
+                relative = 'editor',
+                row = 0.8,
+                col = 0.25,
+                width = 0.9,
+                height = 0.4,
+                border = "single",
+              },
+              horizontal = { location = "rightbelow", split_ratio = .3, },
+              vertical = { location = "rightbelow", split_ratio = .5 },
+            }
+          },
+          behavior = {
+            autoclose_on_quit = {
+              enabled = false,
+              confirm = true,
+            },
+            close_on_exit = true,
+            auto_insert = true,
+          },
+        })
+    end,
   }
 
   -- {
